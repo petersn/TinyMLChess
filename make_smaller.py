229e0135617fb938aa0267f665b6100b04b75ae1
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2018-03-03 12:54:47 -0800

    Adding script to correct a horrific mistake I made.

---
+++ make_smaller.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys, zlib, multiprocessing
+import numpy as np
+
+def process(path):
+	print "Opening up", path
+	with open(path) as f:
+		data = f.read().decode("zlib")
+	array = np.fromstring(data, dtype=np.float64)
+	print "Read in array of size", len(array)
+	del data
+	fixed = array.cast(np.int8).tostring()
+	del array
+	compress = zlib.compressobj(9)
+	output_path = path + "-fixed.z"
+	with open(output_path, "wb") as f:
+		output = compress.compress(fixed)
+		f.write(output)
+		f.write(compress.flush())
+	print "Wrote to:", output_path
+	del fixed
+	del compress
+
+for path in sys.argv[1:]:
+	process(path)
+
+#p = multiprocessing.Pool(4)
+#p.map(process, sys.argv[1:])
+
